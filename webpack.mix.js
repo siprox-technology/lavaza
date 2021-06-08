1cbf8a06d82c899e70292d4cc514335ddc7c9342
Soroosh <integralproject1988@gmail.com>
2021-06-08 09:46:07 +0430

    ok

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,26 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+//css bundle
+mix.combine([
+    "resources/vendor/bootstrap/css/bootstrap.min.css",
+    "resources/vendor/icofont/icofont.min.css",
+    "resources/vendor/boxicons/css/boxicons.min.css",
+    "resources/vendor/animate/animate.min.css",
+    "resources/vendor/venobox/venobox.css",
+    "resources/vendor/owl.carousel/assets/owl.carousel.min.css",
+    'resources/css/style.css'
+], 'public/css/app.css');
+
+//js bundle
+mix.combine([
+    "resources/vendor/jquery/jquery.min.js",
+    "resources/vendor/bootstrap/js/bootstrap.bundle.min.js",
+    "resources/vendor/jquery.easing/jquery.easing.min.js",
+    "resources/vendor/php-email-form/validate.js",
+    "resources/vendor/jquery-sticky/jquery.sticky.js",
+    "resources/vendor/isotope-layout/isotope.pkgd.min.js",
+    "resources/vendor/venobox/venobox.min.js",
+    "resources/vendor/owl.carousel/owl.carousel.min.js",
+    "resources/js/main.js",
+], 'public/js/app.js');
