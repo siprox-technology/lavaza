4e103a1bf80f4e0023fe7eb8d17ca2e1de173b40
Soroosh <integralproject1988@gmail.com>
2021-06-08 10:41:00 +0430

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,29 +10,12 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{asset('images/favicon.png')}}" rel="icon">
-  <link href="{{asset('images/apple-touch-icon.png')}}" rel="apple-touch-icon">
+  <link href="{{asset('assets/img/favicon.png')}}" rel="icon">
+  <link href="{{asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="resources/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="resources/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="resources/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="resources/vendor/animate/animate.min.css" rel="stylesheet">
-  <link href="resources/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="resources/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="resources/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Delicious - v2.2.1
-  * Template URL: https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 
 <body>
@@ -121,18 +104,9 @@
 
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
-  <!-- Vendor JS Files -->
-  <script src="resources/vendor/jquery/jquery.min.js"></script>
-  <script src="resources/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="resources/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="resources/vendor/php-email-form/validate.js"></script>
-  <script src="resources/vendor/jquery-sticky/jquery.sticky.js"></script>
-  <script src="resources/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="resources/vendor/venobox/venobox.min.js"></script>
-  <script src="resources/vendor/owl.carousel/owl.carousel.min.js"></script>
 
   <!-- Template Main JS File -->
-  <script src="resources/js/main.js"></script>
+  <script src="{{asset('js/app.js')}}"></script>
 
 </body>
 
