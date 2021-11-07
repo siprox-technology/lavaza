954dcdbfa5374fa2f76e85db06b2ac5e51af0a73
Soroosh <integralproject1988@gmail.com>
2021-11-07 10:15:37 +0330

    خن

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -189,7 +189,7 @@
                         <div class="col-lg-4 bg-light col-md-6 info text-right">
                             <i class="icofont-google-map"></i>
                             <h4>ادرس</h4>
-                            <p>خیابان  ناصری پلاک ۹۹<br>تهران</p>
+                            <p>خیابان فرهاد ۲۰ پلاک ۸۵<br>مشهد</p>
                         </div>
                         <div class="col-lg-4 bg-light col-md-6 info mt-4 mt-lg-0 text-right">
                             <i class="icofont-clock-time icofont-rotate-90"></i>
@@ -199,7 +199,7 @@
                         <div class="col-lg-4 bg-light col-md-6 info mt-4 mt-lg-0 text-right">
                             <i class="icofont-phone"></i>
                             <h4>تلفن</h4>
-                            <p>+۹۸ ۹۱۵ ۰۰۰۰</p>
+                            <p>+۹۸ ۹۳۷۱۳۷۳۹۲۹</p>
                         </div>
                     </div>
                 </div>
